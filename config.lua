89bb4a3db43897ca1b5e3b17ea8c7eb38ba76a1b
pseudonym-2669 <rob1.tisserand@gmail.com>
2020-06-09 22:18:14 +0200

    version 1.0

---
+++ config.lua
@@ -0,0 +1,4 @@
+WALKABLE_SAND = true
+WALKABLE_DIAMOND = true
+WALKABLE_IRON = true
+test = false
--- init.lua
+++ init.lua
@@ -1,3 +1,8 @@
+dofile(minetest.get_modpath("invisible_block") .. "/config.lua")
+
+
+
+if WALKABLE_SAND then
 minetest.register_node('invisible_block:walkable_sand', {
 	description = "walkable_sand",
 	tiles = {"default_sand.png"},
@@ -9,10 +14,10 @@ minetest.register_node('invisible_block:walkable_sand', {
 	groups = {snappy=0,cracky=0,oddly_unbreakable_by_hand=0},
 	legacy_wallmounted = true,
 })
+end
 
 
-
-
+if WALKABLE_DIAMOND then
 minetest.register_node('invisible_block:walkable_diamond', {
 	description = "walkable diamond",
 	tiles = {"default_diamond_block.png"},
@@ -24,8 +29,10 @@ minetest.register_node('invisible_block:walkable_diamond', {
 	groups = {snappy=0,cracky=0,oddly_unbreakable_by_hand=0},
 	legacy_wallmounted = true,
 })
+end
 
 
+if WALKABLE_IRON then
 minetest.register_node('invisible_block:walkable_iron', {
 	description = "walkable iron",
 	tiles = {"default_steel_block.png"},
@@ -35,9 +42,9 @@ minetest.register_node('invisible_block:walkable_iron', {
 	sunlight_propagates = true,
 	walkable = false,
 	groups = {snappy=0,cracky=0,oddly_unbreakable_by_hand=0},
-	diggable = false,
 	legacy_wallmounted = true,
 })
+end
 
 minetest.register_craft({
 	output = 'invisible_block:walkable_iron',
@@ -61,15 +68,16 @@ minetest.register_craft({
 })
 
 
-
---minetest.register_node('invisible_block:invisible', {
-	--description = "invisible",
-	--tiles = {"invisible_block_invisible.png"},
-	--wield_image = "invisible_block_invisible.png",
-	--paramtype = "light",
-	--paramtype2 = "wallmounted",
-	--sunlight_propagates = true,
-	--walkable = true,
-	--groups = {snappy=0,cracky=0,oddly_unbreakable_by_hand=0},
-	--legacy_wallmounted = false,
---})
+if test then
+minetest.register_node('invisible_block:invisible', {
+	description = "invisible",
+	tiles = {"invisible_block_invisible.png"},
+	wield_image = "invisible_block_invisible.png",
+	paramtype = "light",
+	paramtype2 = "wallmounted",
+	sunlight_propagates = true,
+	walkable = true,
+	groups = {snappy=0,cracky=0,oddly_unbreakable_by_hand=0},
+	legacy_wallmounted = false,
+})
+end
